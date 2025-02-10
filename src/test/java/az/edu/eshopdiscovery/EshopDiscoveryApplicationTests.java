20f85cfe0f5197bfa926133482aa0420bbf9b9e6
nemat-mamiyev <nemet.memiyev1@gmail.com>
2025-02-10 15:15:05 +0400

    first commit

---
+++ src/test/java/az/edu/eshopdiscovery/EshopDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.eshopdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
